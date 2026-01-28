1764de8492abbeaee97c108f5ec68757583e77a2
JonoBos <jonothanb@nycstudents.net>
2026-01-28 12:31:28 -0500

    Create create.js

---
+++ create.js
@@ -0,0 +1,18 @@
+const cards = [
+  { value: 1, item: "banana" },
+  { value: 2, item: "banana" },
+  { value: 3, item: "apple" },
+  { value: 4, item: "apple" },
+  { value: 5, item: "carrot" },
+  { value: 6, item: "carrot" },
+  { value: 7, item: "pear" },
+  { value: 8, item: "pear" },
+  { value: 9, item: "strawberry" },
+  { value: 10, item: "strawerry" },
+  { value: 11, item: "cherry" },
+  { value: 12, item: "cherry" },
+  { value: 13, item: "watermelon" },
+  { value: 14, item: "watermelon" },
+  { value: 15, item: "orange" },
+  { value: 16, item: "orange" }
+];
