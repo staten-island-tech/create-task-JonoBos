391779ecb82ea89107b5faea3f2821422391c123
JonoBos <jonothanb@nycstudents.net>
2026-02-02 11:54:55 -0500

    Update create.js

--- create.js
+++ create.js
@@ -16,3 +16,23 @@ const cards = [
   { value: 15, item: "orange" },
   { value: 16, item: "orange" }
 ];
+
+const choiceOne = []
+const choiceTwo = []
+
+const foundCards = []
+
+//issue: you can only display one card at a time that is viewable to the user, if the second card is clicked there is no delay to hide it
+
+//create a card for each item and display on page attached to a button
+//start the card face down but still linked to there item
+//display the card item as a string once they are clicked (using the button attached)
+//add the first item clicked into an array called choice one
+//add the second item clicked into an array called choice two
+//if display 1 = display 2 , then keep the card item displayed
+//push the 2 items into a new array called foundCards
+// if the cards are already displayed ignore
+//else hide the cards again, clear last item in choice one array and choice two array
+//if foundCards.length = cards.length display you win on a div
+
+document.getElementById
